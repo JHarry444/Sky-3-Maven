c24f707134523f83cd7c3bd4d99219098da2f9bc
jharry444 <jharrison1@qa.com>
2024-07-31 10:11:06 +0100

    init commit

---
+++ src/main/java/com/qa/Main.java
@@ -0,0 +1,11 @@
+package com.qa;
+
+import com.qa.calc.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        System.out.println(Calculator.add(1,2));
+    }
+}
